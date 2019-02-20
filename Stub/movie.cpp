8122febd7fec230459d722920136ce2564c2c56e
Anders Jenbo <anders@jenbo.dk>
2019-02-20 02:45:30 +0100

    hook up video playback

--- Stub/movie.cpp
+++ Stub/movie.cpp
@@ -1,12 +1,17 @@
+#include "../3rdParty/libsmacker/smacker.h"
 #include "../3rdParty/libsmackerdec/include/SmackerDecoder.h"
 #include "../types.h"
 
 BYTE movie_playing;
-BOOL loop_movie;
+BOOL loop_movie; // TODO
 
 void __fastcall play_movie(char *pszMovie, BOOL user_can_close)
 {
 	DUMMY_PRINT("%s", pszMovie);
+
+	SDL_DestroyTexture(texture);
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 320, 156);
+	SDL_RenderSetLogicalSize(renderer, 320, 156);
 	void *video_stream;
 
 	movie_playing = TRUE;
@@ -14,13 +19,44 @@ void __fastcall play_movie(char *pszMovie, BOOL user_can_close)
 	sfx_stop();
 	effects_play_sound("Sfx\\Misc\\blank.wav");
 
-	//SVidPlayBegin(pszMovie, 0, 0, 0, 0, loop_movie ? 0x100C0808 : 0x10280808, &video_stream);
-	if (video_stream) {
-		SDL_Event event;
-		while (video_stream) {
-			if (user_can_close && !movie_playing)
-				break;
+	HANDLE directsound;
+	SFileOpenFile(pszMovie, &directsound);
 
+	char *fileBuffer;
+
+	int bytestoread = SFileGetFileSize(directsound, 0);
+	fileBuffer = DiabloAllocPtr(bytestoread);
+	SFileReadFile(directsound, fileBuffer, bytestoread, NULL, 0);
+
+	/* file meta-info */
+	unsigned int width, height, nFrames;
+	/* arrays for audio track metadata */
+	unsigned char a_trackmask, a_channels[7], a_depth[7];
+	unsigned long a_rate[7];
+
+	unsigned char *palette_data;
+	unsigned char *image_data;
+	unsigned char *audio_data;
+
+	smk smacker;
+
+	smacker = smk_open_memory(fileBuffer, bytestoread);
+	if (smacker != NULL) {
+		int now;
+		double usPerFrame;
+
+		smk_enable_all(smacker, -1);
+		smk_info_all(smacker, NULL, &nFrames, &usPerFrame);
+		smk_info_video(smacker, &width, &height, NULL);
+		smk_info_audio(smacker, &a_trackmask, a_channels, a_depth, a_rate);
+
+		printf("Audio info for track 0: bit-depth %u, channels %u, rate %u\n", a_depth[0], a_channels[0], a_rate[0]);
+
+		smk_first(smacker);
+
+		SDL_Event event;
+		double frameEnd = SDL_GetTicks() * 1000 + usPerFrame;
+		for (int f = 0; f < nFrames && movie_playing; f++) {
 			while (SDL_PollEvent(&event)) {
 				switch (event.type) {
 				case SDL_KEYDOWN:
@@ -31,11 +67,56 @@ void __fastcall play_movie(char *pszMovie, BOOL user_can_close)
 					exit(0);
 				}
 			}
-			//Mix_PlayChannel(-1, pSnd->chunk, 0);
-			video_stream = NULL;
+
+			now = SDL_GetTicks() * 1000;
+			if (now >= frameEnd) {
+				continue; // Skip audio and video if the system is to slow
+			}
+
+			audio_data = smk_get_audio(smacker, 0);
+			SDL_RWops *rw = SDL_RWFromMem(audio_data, smk_get_audio_size(smacker, 0));
+			Mix_Chunk *SoundFX = Mix_LoadWAV_RW(rw, 0);
+			Mix_PlayChannel(-1, SoundFX, 0);
+
+			now = SDL_GetTicks() * 1000;
+			if (now >= frameEnd) {
+				continue; // Skip video if system is to slow
+			}
+
+			/* Retrieve the palette and image */
+			palette_data = smk_get_palette(smacker);
+			image_data = smk_get_video(smacker);
+			for (int i = 0; i < 256; i++) {
+				orig_palette[i].peFlags = 0;
+				orig_palette[i].peRed = palette_data[i * 3 + 0];
+				orig_palette[i].peGreen = palette_data[i * 3 + 1];
+				orig_palette[i].peBlue = palette_data[i * 3 + 2];
+			}
+			ApplyGamma(logical_palette, orig_palette, 256);
+
+			// Copy frame to buffer
+			BYTE *src = (BYTE *)image_data;
+			BYTE *dst = (BYTE *)&gpBuffer->row[0].pixels[0];
+			for (int i = 0; i < height && i < SCREEN_HEIGHT; i++, src += width, dst += ROW_PITCH) {
+				for (int j = 0; j < width && j < SCREEN_WIDTH; j++) {
+					dst[j] = src[j];
+				}
+			}
+
+			SetFadeLevel(256); // present frame
+
+			smk_next(smacker); // Decode next frame
+			now = SDL_GetTicks() * 1000;
+			if (now < frameEnd) {
+				usleep(frameEnd - now); // wait with next frame if the system is to fast
+			}
+			frameEnd += usPerFrame;
 		}
-		//if (video_stream)
-			//SVidPlayEnd(video_stream);
+
+		smk_close(smacker);
 	}
-	//sound_disable_music(FALSE);
+
+	SDL_DestroyTexture(texture);
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
